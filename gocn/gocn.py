78a0fb274d0f52981e522c74e7f6adc6607fa801
ghostry <ghostry@ghostry.cn>
2019-03-22 05:33:03 +0800

    添加域名

--- gocn/gocn.py
+++ gocn/gocn.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python  
-#coding=utf-8
+#!/usr/bin/env python
+# coding=utf-8
 import re
 import os
 
 ipset_forward = 'govpn'
 rulesfile = './dnsmasq.conf'
-domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
+domain_pattern = '^([\w\-\_\.]*[\w\.\-\_]+)'
 domainlist = open('domain.list', 'r')
 try:
-	os.remove(rulesfile)
+    os.remove(rulesfile)
 except:
-	pass
+    pass
 fs = open(rulesfile, 'w')
 for line in domainlist.readlines():
-	domain = re.findall(domain_pattern, line)
-	if domain:
-		print('saving ' + domain[0])
-		fs.write('ipset=/.%s/%s\n'%(domain[0],ipset_forward))
-	else:
-		print 'no valid domain in this line: ' + line
+    domain = re.findall(domain_pattern, line)
+    if domain:
+        print('saving ' + domain[0])
+        fs.write('ipset=/.%s/%s\n' % (domain[0], ipset_forward))
+    else:
+        print 'no valid domain in this line: ' + line
 domainlist.close()
 fs.close()
-
-
