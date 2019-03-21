9cd97506cab06e474a262bab6450acc062669fa6
ghostry <ghostry@ghostry.cn>
2019-03-21 21:22:48 +0800

    up

--- gocn/up1.1.sh
+++ gocn/up1.1.sh
@@ -2,6 +2,8 @@
 #天朝网站走回国VPN
 #生成DNSmasq的ipset设置，并上传到路由器
 #然后在mwan3配置规则
+#路由器添加启动项
+#ipset create govpn hash:ip
 python gocn.py
 scp dnsmasq.conf root@192.168.1.1:/etc/dnsmasq.conf
 ssh root@192.168.1.1 '/etc/init.d/dnsmasq restart;'
