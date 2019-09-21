bcfd1fc5084a9b98a4520cc035299c005cf39a56
seaze <seaze@se.cn>
2019-09-21 13:34:30 +0800

    fix

--- openvpn/createConfig.sh
+++ openvpn/createConfig.sh
@@ -2,7 +2,7 @@
 cd $(dirname $(readlink -f "$0"))
 image="kylemanna/openvpn"
 DNSNAME=`curl ipinfo.io/ip`
-docker run -v $PWD:/etc/openvpn --rm $image ovpn_genconfig -u udp://$DNSNAME -b
-docker run -v $PWD:/etc/openvpn --rm -it $image ovpn_initpki
-docker run -v $PWD:/etc/openvpn --rm -it $image easyrsa build-client-full CLIENTNAME nopass
-docker run -v $PWD:/etc/openvpn --rm $image ovpn_getclient CLIENTNAME >$DNSNAME.ovpn
+docker run -v $PWD:/etc/openvpn --rm --user $(id -u):$(id -g) $image ovpn_genconfig -u udp://$DNSNAME -b
+docker run -v $PWD:/etc/openvpn --rm --user $(id -u):$(id -g) -it $image ovpn_initpki
+docker run -v $PWD:/etc/openvpn --rm --user $(id -u):$(id -g) -it $image easyrsa build-client-full CLIENTNAME nopass
+docker run -v $PWD:/etc/openvpn --rm --user $(id -u):$(id -g) $image ovpn_getclient CLIENTNAME >$DNSNAME.ovpn
