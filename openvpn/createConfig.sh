dc06e7dd8260588ffb0a63392c76fb3214d85bac
seaze <seaze@se.cn>
2019-09-21 13:30:47 +0800

    up

--- openvpn/createConfig.sh
+++ openvpn/createConfig.sh
@@ -5,4 +5,4 @@ DNSNAME=`curl ipinfo.io/ip`
 docker run -v $PWD:/etc/openvpn --rm $image ovpn_genconfig -u udp://$DNSNAME -b
 docker run -v $PWD:/etc/openvpn --rm -it $image ovpn_initpki
 docker run -v $PWD:/etc/openvpn --rm -it $image easyrsa build-client-full CLIENTNAME nopass
-docker run -v $PWD:/etc/openvpn --rm $image ovpn_getclient CLIENTNAME >CLIENTNAME.ovpn
+docker run -v $PWD:/etc/openvpn --rm $image ovpn_getclient CLIENTNAME >$DNSNAME.ovpn
