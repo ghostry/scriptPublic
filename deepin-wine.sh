0520e0a7325b803c23983dfdd13a021c0a8c54f5
momo <ghostry@ghostry.cn>
2019-12-01 10:49:50 +0800

    取消提示

--- deepin-wine.sh
+++ deepin-wine.sh
@@ -3,6 +3,6 @@ filename="/etc/apt/sources.list.d/deepin-wine-install.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 425956BB3E31DF51
 echo "deb http://mirrors.aliyun.com/deepin/ stable contrib main non-free"|sudo tee $filename
 sudo apt-get update
-sudo apt-get install deepin-wine
+sudo apt-get install -y deepin-wine
 sudo rm $filename
 sudo apt-get update
