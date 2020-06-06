c5d79252812fa3a3b42c802426fc4741e5d68d20
ghostry <ghostry@ghostry.cn>
2020-06-06 08:38:21 +0800

    Update deepin-wine.sh

--- deepin-wine.sh
+++ deepin-wine.sh
@@ -3,7 +3,7 @@ filename="/etc/apt/sources.list.d/deepin-wine-install.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 425956BB3E31DF51
 echo "deb https://mirrors.huaweicloud.com/deepin stable main contrib non-free"|sudo tee $filename
 sudo apt-get update
-sudo apt-get install -y deepin-wine
+# 更多软件名查看 https://mirrors.huaweicloud.com/deepin/pool/non-free/d/
 sudo apt-get install -y deepin.com.wechat
 sudo apt-get install -y deepin.com.qq.office
 sudo rm $filename
