9690d974411c565c61e03b69e768aa571395187b
adams <ghostry@ghostry.cn>
2020-06-22 03:51:35 +0800

    修改

--- gnome-shell-theme.gresource/3.install.sh
+++ gnome-shell-theme.gresource/3.install.sh
@@ -4,9 +4,8 @@ mytheme="/usr/share/gnome-shell/theme/mytheme"
 gs="${mytheme}/gnome-shell-theme.gresource"
 if [ ! -d ${mytheme} ]; then
     sudo cp -r ${Yaru} ${mytheme}
-    sudo rm ${gs}
 fi
-sudo mv gnome-shell-theme.gresource $gs
+sudo mv -f gnome-shell-theme.gresource $gs
 sudo update-alternatives --install /usr/share/gnome-shell/gdm3-theme.gresource gdm3-theme.gresource $gs 16
 sudo update-alternatives --auto gdm3-theme.gresource
 sudo update-alternatives --config gdm3-theme.gresource
--- gnome-shell-theme.gresource/README.md
+++ gnome-shell-theme.gresource/README.md
@@ -1,5 +1,7 @@
 # 修改Ubuntu 20.04 登陆界面
 
+该脚本工具会从 `Yaru` 主题复制出一个新主题 `mytheme` 并安装使用
+
 1. cd到当前目录,执行`./1.extractgst.sh`
 2. 在`gnome-shell-theme`目录中进行一些修改,比如修改背景,`gdm3.css`中的`#lockDialogGroup{...}`修改为`#lockDialogGroup{background-color:rgba(27,106,203,0.3);background-image:url('background.png');background-position:center;background-repeat:no-repeat;background-size:cover;background-attachment:fixed;}`,并且放置`background.png`到`gnome-shell-theme`
 3. 执行`2.create.gnome-shell-theme.gresource.sh`
