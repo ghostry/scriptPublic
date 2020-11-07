08a0f3adda9cd97e04461a9ebf0769eab1226b91
adams <ghostry@ghostry.cn>
2020-11-07 20:15:25 +0800

    修改方式

--- deepin-wine.sh
+++ deepin-wine.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-filename="/etc/apt/sources.list.d/deepin-wine-install.list"
+filename="/etc/apt/sources.list.d/deepin.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 425956BB3E31DF51
-echo "deb https://mirrors.huaweicloud.com/deepin stable main contrib non-free"|sudo tee $filename
+echo "deb [by-hash=force] https://community-packages.deepin.com/deepin/ apricot main contrib non-free"|sudo tee $filename
+echo "Package: *\nPin: origin community-packages.deepin.com\nPin-Priority: 200"|sudo tee /etc/apt/preferences.d/deepin
 sudo apt-get update
-# 更多软件名查看 https://mirrors.huaweicloud.com/deepin/pool/non-free/d/
+# 更多软件名查看 https://community-packages.deepin.com/deepin/pool/non-free/d/
 sudo apt-get install -y deepin.com.wechat
 sudo apt-get install -y deepin.com.qq.office
-sudo rm $filename
-sudo apt-get update
